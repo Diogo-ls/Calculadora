e795d0d03c75dad0621115cb103ccddb6136d5fe
diogo leal <diogoleal12@gmail.com>
2021-09-11 12:19:08 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './screens/home';
 function App() {
   return (
     <div>
-     <Home/>
+     <Home />
     </div>
   );
 }
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { evaluate } from 'mathjs';
 
 const Home = () => {
-    const [value, setValue ] = useState('') 
+    const [value, setValue ] = useState('')
     function calculate() {
         let newValue= evaluate(value);
         setValue(newValue)
@@ -10,8 +10,10 @@ const Home = () => {
     function clean() {
         setValue('')
     }
-    return ( 
+    return (
+         
         <div>
+            <h1>Calculadora</h1>
             {value}
             <br/>
             <input type='button' value='1' onClick={(e) => setValue(value + e.target.value)} />
@@ -31,11 +33,9 @@ const Home = () => {
             <input type='button' value='*' onClick={(e) => setValue(value + e.target.value)} />
             <input type='button' value='/' onClick={(e) => setValue(value + e.target.value)} />
             <br/>
+            <input type='button' value='0' onClick={(e) => setValue(value + e.target.value)} />
             <input type='button'  value='calcular' onClick={() => calculate()} />
             <input type='button'  value='limpar' onClick={() => clean()} />
-
-
-
         </div>
     )
 }
